0e88633f5b9b5bcdc2493f03850b12db549fffc0
강다영 <gadin8631@naver.com>
2024-07-25 14:32:27 +0900

    Initial commit

---
+++ src/main/java/com/haribo/cscenter_service/CscenterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.haribo.cscenter_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CscenterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CscenterServiceApplication.class, args);
+    }
+
+}
